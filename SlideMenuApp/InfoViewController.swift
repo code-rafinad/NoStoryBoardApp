cdb44e2bbdb7492ff3c8574b73f1ca30210df290
Rafinad <rafinad@outlook.com>
2021-05-04 14:16:02 +0300

    colour corrections

--- SlideMenuApp/InfoViewController.swift
+++ SlideMenuApp/InfoViewController.swift
@@ -13,7 +13,7 @@ class InfoViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        view.backgroundColor = .systemGray6
         title = "Info"
 
     }
